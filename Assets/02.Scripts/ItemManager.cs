878e7dde2c06fd8f26386d1ad54c0de39bd6d945
OhJaeWook <decisive20@gmail.com>
2024-05-27 16:59:34 +0900

    아이템 시스템 구현 및 미사용 스크립트 삭제

---
+++ Assets/02.Scripts/ItemManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    public List<Item> itemDatabase = new List<Item>();
+
+    void Start()
+    {
+        // 임시 아이템 데이터베이스 초기화 (나중에 실제 데이터로 대체)
+        itemDatabase.Add(new Item(1, "고유 아이템 1", "설명 1", null));
+        itemDatabase.Add(new Item(2, "고유 아이템 2", "설명 2", null));
+        itemDatabase.Add(new Item(3, "고유 아이템 1", "설명 1", null));
+        itemDatabase.Add(new Item(4, "고유 아이템 2", "설명 2", null));
+        itemDatabase.Add(new Item(5, "고유 아이템 1", "설명 1", null));
+        // 필요한 만큼 추가...
+    }
+
+    public Item GetItemById(int id)
+    {
+        return itemDatabase.Find(item => item.id == id);
+    }
+}
---
+++ Assets/02.Scripts/ItemPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public int itemId;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerInventory inventory = other.GetComponent<PlayerInventory>();
+            if (inventory != null)
+            {
+                inventory.AddItem(itemId);
+                Destroy(gameObject); // 아이템 오브젝트 제거
+            }
+        }
+    }
+}
